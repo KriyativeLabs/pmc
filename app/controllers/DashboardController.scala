12e94dded0cc9970a717f0e6fa0cae0a18abaced
Suryaprabhakar <suryap@collective.com>
2015-12-02 02:36:23 +0530

    un reachable msg

--- app/controllers/DashboardController.scala
+++ app/controllers/DashboardController.scala
@@ -13,24 +13,24 @@ import security.{IsAuthenticated, PermissionCheckAction}
 object DashboardController extends Controller with DashboardSerializer with CommonUtil with ResponseHelper {
   val logger = Logger(this.getClass)
 
-  def dashboardData = (IsAuthenticated andThen PermissionCheckAction(UserType.OWNER)) { implicit request =>
+  def dashboardData = (IsAuthenticated andThen PermissionCheckAction(UserType.AGENT)) { implicit request =>
     val result = Companies.dashboardData(request.user.companyId)
     println(result)
     ok(Json.toJson(result),"DashboardData")
   }
 
-  def notifications = (IsAuthenticated andThen PermissionCheckAction(UserType.OWNER)) { implicit request =>
+  def notifications = (IsAuthenticated andThen PermissionCheckAction(UserType.AGENT)) { implicit request =>
     implicit val loggedInUser = request.user
     val result = Notifications.all()
     ok(Json.toJson(result),"Notifications")
   }
 
-  def monthlyStatistics = (IsAuthenticated andThen PermissionCheckAction(UserType.OWNER)) { implicit request =>
+  def monthlyStatistics = (IsAuthenticated andThen PermissionCheckAction(UserType.AGENT)) { implicit request =>
     val result = CompanyStats.getLatestByCount(request.user.companyId)
     ok(Json.toJson(result),"Monthly Statistics")
   }
 
-  def agentStatistics = (IsAuthenticated andThen PermissionCheckAction(UserType.OWNER)) { implicit request =>
+  def agentStatistics = (IsAuthenticated andThen PermissionCheckAction(UserType.AGENT)) { implicit request =>
     val result = CompanyStats.getAgentStatistics(request.user.companyId)
     ok(Json.toJson(result),"Agent Statistics")
   }
