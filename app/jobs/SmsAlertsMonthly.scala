1afee2731f15b3686e22079d132b6b36a3642b7b
Suryaprabhakar <suryap@collective.com>
2015-12-02 01:19:10 +0530

    cron jobs

---
+++ app/jobs/SmsAlertsMonthly.scala
@@ -0,0 +1,23 @@
+package jobs
+
+import models._
+import org.quartz.{Job, JobExecutionContext}
+import play.api.Logger
+
+class SmsAlertsMonthly extends Job {
+  override def execute(context: JobExecutionContext) {
+    Companies.getAll.foreach({ company =>
+      Logger.info(s"Sms Generation started for company:${company.id}")
+      try {
+        Customers.getAll(company.id.get).foreach({ customer =>
+          if (customer.customer.balanceAmount > 0 && customer.customer.mobileNo.isDefined) {
+            val message = s"Dear Customer, You cable connection pending balance is:${customer.customer.balanceAmount}. Please pay to our agent to avoid disconnection."
+            SmsGateway.sendSms(message, customer.customer.mobileNo)
+          }
+        })
+      } catch {
+        case e: Exception => Logger.info(s"Exception caught in processing bill generation for company:${company.id}", e)
+      }
+    })
+  }
+}
