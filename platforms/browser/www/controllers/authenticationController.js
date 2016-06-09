2d3709a461319d3804e50af727899f91097a96f7
Suryaprabhakar <suryap@collective.com>
2016-06-09 22:30:42 +0530

    added loader for login

--- platforms/browser/www/controllers/authenticationController.js
+++ platforms/browser/www/controllers/authenticationController.js
@@ -3,22 +3,26 @@ pmcApp.controller('authenticationController', ['$scope','$window', 'cookieServic
         console.log("authentication controller loaded");
         $scope.isError = false;
         $scope.error = "";
+        $scope.isLoading = false;
         if(cookieService.get(constantsService.TOKEN)){
             $window.location.href = "index.html";
         }
         $scope.login = function(){
+            $scope.isLoading = true;
             apiService.POST("/login?account_type=cable",'{"loginId":"'+$scope.loginid+'","password":"'+$scope.password+'"}').then(function(response){
                 apiService.NOTIF_SUCCESS(response.data.message);
                 cookieService.set(constantsService.TOKEN,response.data.data.token);
                 cookieService.set(constantsService.USERNAME, response.data.data.name);
                 cookieService.set(constantsService.COMPANY_NAME, response.data.data.company);
                 cookieService.set(constantsService.ACC_TYPE, response.data.data.type);
+                $scope.isLoading = false;
                 $window.location.href = "index.html";
             },function(errorResponse){
-		console.log(errorResponse)
+                $scope.isLoading = false;
+		        console.log(errorResponse)
                 if(errorRespose.status !=200){
                     $scope.isError = true;
-                    $scope.error = "Login failed due to incorrect login and password!"
+                    $scope.error = "Login failed due to incorrect login and password!";
                     apiService.NOTIF_ERROR($scope.error);
                 }
             });
